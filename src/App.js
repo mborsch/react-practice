381a81989dcd3264c10cfdafed18b291a8089a9e
mborsch <matthewmborschnack@gmail.com>
2022-05-26 13:27:45 -0500

    added users

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostDetail from "./PostDetail";
 function App() {
   const [posts, setPosts] = useState([]);
   const [postId, setPostId] = useState("");
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     async function loadPosts() {
@@ -16,10 +17,30 @@ function App() {
     loadPosts();
   }, []);
 
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users`
+      );
+      const usersFromAPI = await response.json();
+      setUsers(usersFromAPI);
+    }
+    loadUsers();
+  }, []);
+
   function idHandler(e) {
     e.preventDefault();
     setPostId(e.target.id);
   }
+
+  const userMap = users.map((user, index) => (
+    <div key={index}>
+      <h2>
+        {user.name} - {user.id}
+      </h2>
+      <p>{user.email}</p>
+    </div>
+  ));
 
   const postMap = posts.map((post, index) => (
     <div key={index} postId={post.id}>
@@ -35,6 +56,7 @@ function App() {
   return (
     <div className="App">
       <div>{postMap}</div>
+      <div>{userMap}</div>
       {postId.length > 0 ? <PostDetail postId={postId} /> : null}
     </div>
   );
